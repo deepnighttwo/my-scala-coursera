fe174eba733b0ee122e2948669b78624cfe0c040
mzang <mzang@paypal.com>
2018-05-10 16:45:47 +0800

    add homework for week1

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.collection.immutable
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -13,6 +15,8 @@ object Main {
     println(balance("I told him (that it’s not (yet) done). (But he wasn’t listening)".toList))
     println(balance(":-)".toList))
     println(balance("())(".toList))
+
+    println(countChange(4, List(1, 2)))
   }
 
   /**
@@ -33,23 +37,75 @@ object Main {
     * Exercise 2
     */
   def balance(chars: List[Char]): Boolean = {
-
-    def reduceNum(a: Int, b: Int): Int = {
-      if (a >= 0) {
-        a + b
+    def balanceIt(left: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) {
+        left == 0
       } else {
-        -1
+        if (chars.head == '(') {
+          balanceIt(left + 1, chars.tail)
+        } else {
+          if (left == 0) {
+            false
+          } else {
+            balanceIt(left - 1, chars.tail)
+          }
+        }
       }
     }
 
-    chars.filter(ch => ch == '(' || ch == ')').map {
-      case '(' => 1
-      case ')' => -1
-    }.reduceLeft(reduceNum) == 0
+    balanceIt(0, chars.filter(ch => ch == '(' || ch == ')'))
+
   }
 
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    val maxParams = coins.map(money / _)
+    val maxParamsRev = coins.map(money / _).reverse
+
+    def nextParam(param: List[Int]) = {
+      var delta = -1
+      val p = for (
+        pair <- param.reverse.zip(maxParamsRev)
+      ) yield {
+        val i = pair._1
+        val max = pair._2
+        if (delta == 0) {
+          i
+        } else {
+          val v = i + delta
+          delta = 0
+          if (v < 0) {
+            delta = -1
+            max
+          } else {
+            v
+          }
+        }
+      }
+      if (p.head < 0 || delta == -1) {
+        Nil
+      } else {
+        val real = p.reverse
+        real
+      }
+    }
+
+    def countChangeIt(total: Int, params: List[Int]): Int = {
+      if (params == Nil) {
+        total
+      } else {
+        val v = coins.zip(params).map { case (a, b) => a * b }.sum
+        val newParam = nextParam(params)
+        if (v == money) {
+          countChangeIt(total + 1, newParam)
+        } else {
+          countChangeIt(total, newParam)
+        }
+      }
+    }
+
+    countChangeIt(0, maxParams)
+  }
 }
